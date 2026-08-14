3ff0202b1ecb018a440f53d0a78488302a98fef0
XxdMkbMark <103048057+XxdMkbMark@users.noreply.github.com>
2026-08-14 20:00:37 +0800

    added more ui

--- desktopApp/src/main/kotlin/cc/lanternmc/materiallauncher/ui/pages/JavaSettings.kt
+++ desktopApp/src/main/kotlin/cc/lanternmc/materiallauncher/ui/pages/JavaSettings.kt
@@ -1,18 +1,96 @@
 package cc.lanternmc.materiallauncher.ui.pages
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun JavaSettingsPage(navController: NavHostController) {
+    val javaList = listOf("17.0", "21.0", "25.0", "1.8.0")
+    var sliderPosition by remember { mutableStateOf(0f..100f) }
+    var expanded by remember { mutableStateOf(false) }    // 状态
+    var selectedOption by remember { mutableStateOf(javaList[0]) }
     MaterialTheme {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Text("Java Settings")
+        Box(modifier = Modifier.fillMaxSize().padding(16.dp,16.dp)) {
+            Column {
+                Text(text = "全局Java版本", modifier = Modifier.padding(4.dp,10.dp))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.End),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    ExposedDropdownMenuBox(
+                        expanded = expanded,
+                        onExpandedChange = { expanded = it }
+                    ) {
+                        // 文本框
+                        TextField(
+                            value = selectedOption,
+                            onValueChange = {},
+                            readOnly = true,
+                            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                            modifier = Modifier.menuAnchor()     // 将文本框与菜单锚定
+                        )
+
+                        // 下拉菜单
+                        ExposedDropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+                            javaList.forEach { java ->
+                                DropdownMenuItem(
+                                    text = { Text(java) },
+                                    onClick = {
+                                        selectedOption = java
+                                        expanded = false
+                                    }
+                                )
+                            }
+                        }
+                    }
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.padding(12.dp,0.dp)
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.End)
+                        ) {
+                            Icon(imageVector = Icons.Rounded.Refresh, contentDescription = "")
+                            Text("重新扫描")
+                        }
+                    }
+                }
+                Text(text = "全局JVM内存分配", modifier = Modifier.padding(4.dp,16.dp))
+
+            }
         }
     }
 }
